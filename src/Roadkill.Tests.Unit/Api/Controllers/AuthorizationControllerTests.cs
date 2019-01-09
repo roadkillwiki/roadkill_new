24a199620e17343ff54b46aa795be5fd4b2540a2
Chris Small <chris.small@just-eat.com>
2019-01-09 08:50:44 +0000

    Auto stash before rebase of "origin/master"

---
+++ src/Roadkill.Tests.Unit/Api/Controllers/AuthorizationControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoFixture;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Roadkill.Api.Common.Models;
+using Roadkill.Api.Controllers;
+using Roadkill.Core.Authorization;
+using Shouldly;
+using Xunit;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace Roadkill.Tests.Unit.Api.Controllers
+{
+	public sealed class AuthorizationControllerTests
+	{
+		private readonly Fixture _fixture;
+		private AuthorizationController _authorizationController;
+		private UserManager<RoadkillUser> _userManagerMock;
+		private SignInManager<RoadkillUser> _signinManagerMock;
+
+		public AuthorizationControllerTests()
+		{
+			_fixture = new Fixture();
+			_userManagerMock = Substitute.For<UserManager<RoadkillUser>>();
+			_signinManagerMock = Substitute.For<SignInManager<RoadkillUser>>();
+
+			_authorizationController = new AuthorizationController(_userManagerMock, _signinManagerMock);
+		}
+
+		[Fact]
+		public async Task GetAll_should_return_all_users_from_manager()
+		{
+			// given
+			var signInResult = SignInResult.Success;
+			string email = "admin@example.org";
+			string password = "labrador";
+			var roadkillUser = new RoadkillUser()
+			{
+				Email = email,
+				NormalizedEmail = email.ToUpperInvariant()
+			};
+
+			var model = new AuthenticationModel()
+			{
+				Email = email,
+				Password = password
+			};
+
+			var claimsList = new List<Claim>()
+			{
+				new Claim(ClaimTypes.Role, "Admin")
+			};
+
+			_userManagerMock.FindByEmailAsync(email).Returns(roadkillUser);
+			_signinManagerMock.PasswordSignInAsync(roadkillUser, password, true, false).Returns(signInResult);
+			_userManagerMock.GetClaimsAsync(roadkillUser).Returns(claimsList);
+
+			// Inject SecurityTokenHandler
+
+			// when
+			OkObjectResult actionResult = await _authorizationController.Authenticate(model) as OkObjectResult;
+
+			// then
+			actionResult.ShouldNotBeNull();
+			actionResult.Value.ShouldBeOfType<string>();
+			actionResult.Value.ToString().ShouldNotBeNullOrWhiteSpace();
+		}
+	}
+}
---
+++ src/Roadkill.Tests.Unit/Api/Controllers/UsersControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using Roadkill.Api.Controllers;
+using Roadkill.Core.Authorization;
+using Shouldly;
+using Xunit;
+
+namespace Roadkill.Tests.Unit.Api.Controllers
+{
+	public sealed class UsersControllerTests
+	{
+		private readonly Fixture _fixture;
+		private UsersController _usersController;
+		private UserManager<RoadkillUser> _userManagerMock;
+		private SignInManager<RoadkillUser> _signinManagerMock;
+
+		public UsersControllerTests()
+		{
+			_fixture = new Fixture();
+			_userManagerMock = Substitute.For<UserManager<RoadkillUser>>();
+			_signinManagerMock = Substitute.For<SignInManager<RoadkillUser>>();
+
+			_usersController = new UsersController(_userManagerMock, _signinManagerMock);
+		}
+
+		[Fact]
+		public async Task GetAll_should_return_all_users_from_manager()
+		{
+			// given
+			var expectedAllUsers = _fixture.CreateMany<RoadkillUser>(5);
+
+			_userManagerMock.Users.Returns(expectedAllUsers);
+
+			// when
+			IEnumerable<RoadkillUser> actualAllUsers = await _usersController.GetAll();
+
+			// then
+			actualAllUsers.Count().ShouldBe(actualAllUsers.Count());
+		}
+	}
+}
