3433e343bad7acee135331873c8891b5b6a5e249
Chris Small <chris.small@just-eat.com>
2020-01-21 12:50:26 +0000

    Web: use double quotes

--- src/web/src/components/ComponentExample.js
+++ src/web/src/components/ComponentExample.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ComponentExample extends Component
 {
--- src/web/src/index.js
+++ src/web/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
