cd19980c601bf193025d582af96c97fadb991f9e
Chris Small <chris.small@just-eat.com>
2020-01-31 12:25:19 +0000

    React: add roadkill-api-js and small test

--- src/web/src/containers/App.js
+++ src/web/src/containers/App.js
@@ -5,8 +5,25 @@ import FunctionalComponent from "../components/FunctionalComponent";
 import PageComponent from "../components/PageComponent";
 
 import './App.css';
+import * as RoadkillApi from "roadkill-api-js";
 
 function App() {
+
+  var defaultClient = RoadkillApi.ApiClient.instance;
+  defaultClient.basePath = "http://localhost:5001";
+
+  let apiInstance = new RoadkillApi.AuthorizationApi(defaultClient);
+  let authorizationRequest = new RoadkillApi.AuthorizationRequest();
+  authorizationRequest.email = "admin@example.org";
+  authorizationRequest.password = "Password12345678901";
+  apiInstance.authorizationAuthenticate(authorizationRequest, (error, data, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+  });
+
   return (
     <div className="App">
         Not much here for now. Some components:
